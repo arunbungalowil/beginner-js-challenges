2b1406ddad79b1d9354a3e4d7775e2db6b9c7183
arun babu <ntarunbabu@gmail.com>
2024-07-20 09:39:21 +0100

    finished the javascript and css part of the challenge. did a basic email validation using javascript. also provided styles to handle the error

---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('form');
+    const emailInput = document.getElementById('email-field')
+    const error = document.getElementById('error-message');
+
+    emailInput.classList.remove('error-state')
+    form.addEventListener('submit', function(event){
+        let emailValue = emailInput.value.trim();
+        let hasError = false;
+        const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailValue){
+            event.preventDefault();
+            emailInput.classList.add('error-state');
+            error.textContent = 'email required';
+            error.style.visibility = 'visible'
+            hasError = true;
+        }else if(emailValue && !emailregex.test(emailValue)){
+            event.preventDefault();
+            emailInput.classList.add('error-state');
+            error.textContent = 'valid email required';
+            error.style.visibility = 'visible'
+            hasError = true;
+        }
+
+        if(hasError == false){
+            emailInput.classList.remove('error-state');
+            error.style.visibility = 'hidden'
+        }
+    });
+
+});
