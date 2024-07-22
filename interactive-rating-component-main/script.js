605d4929a09a5881b75877ee06e42895ad1eb326
arun babu <ntarunbabu@gmail.com>
2024-07-22 01:27:17 +0100

    finished the js part of the challenge. this challenge is a relatively simple challenge

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('rating-form');
+    const buttons = document.querySelectorAll('.btn');
+    let selectedRating = null;
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event){
+            selectedRating = this.value;
+            console.log(selectedRating)
+            // event.preventDefault();
+        });  
+    })
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        if(selectedRating === null){
+            event.preventDefault();
+            alert('select a rating')
+        }else{
+            let redirectURL = 'thankyou.html?rating='+selectedRating;
+            console.log(redirectURL)
+            window.location.href = redirectURL;
+        }
+        
+    });
+
+});
