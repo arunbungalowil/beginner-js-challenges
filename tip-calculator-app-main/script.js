4d38de888a20ea9a497d33d057001c62ddf1c5ec
arun babu <ntarunbabu@gmail.com>
2024-07-24 11:41:43 +0100

    finished the js part of the project

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -8,64 +8,66 @@ document.addEventListener('DOMContentLoaded', function(){
     const percentageButtons = document.querySelectorAll('.bill-tip');
     const customTip = document.getElementById('bill-tip-6'); 
 
+    tipAmountPerPerson.textContent = '0.00';
+    totalTipPerPerson.textContent = '0.00';
+
     let amount = 0;
     let totalPerson = 0;
     let tipPercentage = 0;
 
-    function calculateTipAmount() {
-        if (amount > 0 && totalPerson > 0 && tipPercentage >= 0) {
-            const yourTotalTipAmount = (amount * tipPercentage) / 100;
-            const perPersonCalculation = yourTotalTipAmount / totalPerson;
-            tipAmountPerPerson.textContent = yourTotalTipAmount.toFixed(2);
-            totalTipPerPerson.textContent = perPersonCalculation.toFixed(2);
-        }
-    }
+    function calculateTipAmount(){
+        if(amount > 0 && totalPerson > 0 && tipPercentage >= 0){
+            const totalTip = amount * (tipPercentage / 100);
+            const perPersonTipAmount = totalTip / totalPerson;
+            totalTipPerPerson.textContent = totalTip.toFixed(2);
+            tipAmountPerPerson.textContent = perPersonTipAmount.toFixed(2);
+        }  
+    } 
 
-    function validateInputs() {
-        if (totalPerson === 0 || isNaN(totalPerson)) {
-            numberOfPeople.style.border = 'solid 1px red';
+    function validNumberOfPerson(){
+        if(totalPerson === 0 || isNaN(totalPerson)){
             errorMessage.style.visibility = 'visible';
-        } else {
-            numberOfPeople.style.border = 'none';
+            numberOfPeople.style.border = 'solid 1px red';
+        }else{
             errorMessage.style.visibility = 'hidden';
+            numberOfPeople.style.border = 'none';
         }
     }
 
     billAmount.addEventListener('input', function(){
         amount = parseFloat(billAmount.value);
-        calculateTipAmount();
     });
 
     numberOfPeople.addEventListener('input', function(){
         totalPerson = parseInt(numberOfPeople.value);
-        validateInputs();
+        validNumberOfPerson();
         calculateTipAmount();
     });
 
-    percentageButtons.forEach(button => {
+   percentageButtons.forEach(button=>{
         button.addEventListener('click', function(){
-            tipPercentage = parseFloat(button.value);
-            validateInputs();
+            tipPercentage = parseInt(button.value);
+            validNumberOfPerson();
             calculateTipAmount();
         });
-    });
-
-    customTip.addEventListener('input', function(){
+   });
+   customTip.addEventListener('input', function(){
         tipPercentage = parseFloat(customTip.value);
-        validateInputs();
+        validNumberOfPerson();
         calculateTipAmount();
-    });
+   });
 
-    resetButton.addEventListener('click', function(){
+   resetButton.addEventListener('click', function(){
         billAmount.value = '';
         numberOfPeople.value = '';
         customTip.value = '';
         tipAmountPerPerson.textContent = '0.00';
         totalTipPerPerson.textContent = '0.00';
-        numberOfPeople.style.border = 'none';
         errorMessage.style.visibility = 'hidden';
+        numberOfPeople.style.border = 'none';
         amount = 0;
         totalPerson = 0;
         tipPercentage = 0;
-    });
+   });
+
 });
